2453f43f48d7729e1872e15b16cac0a490502a57
xuwu / luna <loganalexzand@gmail.com>
2024-04-21 02:46:16 -0800

    Complete Wipe all out rewrite mode

--- src/main/java/uwu/lopyluna/create_dd/DDCreate.java
+++ src/main/java/uwu/lopyluna/create_dd/DDCreate.java
@@ -11,27 +11,14 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
-import uwu.lopyluna.create_dd.block.BlockProperties.flywheel.engine.FurnaceEngineBlock;
-import uwu.lopyluna.create_dd.block.BlockResources.DDBlockPartialModel;
-import uwu.lopyluna.create_dd.block.DDBlocks;
-import uwu.lopyluna.create_dd.block.DDBlockEntityTypes;
-import uwu.lopyluna.create_dd.block.BlockPalette.DDPaletteBlocks;
-import uwu.lopyluna.create_dd.configs.DDConfigs;
-import uwu.lopyluna.create_dd.fluid.DDFluids;
-import uwu.lopyluna.create_dd.item.DDItems;
-import uwu.lopyluna.create_dd.creative.DDItemTab;
-import uwu.lopyluna.create_dd.recipe.DDRecipesTypes;
-import uwu.lopyluna.create_dd.sounds.DDSoundEvents;
 
 
 @SuppressWarnings({"all"})
@@ -48,7 +35,7 @@ public class DDCreate
     public static KineticStats create(Item item) {
         if (item instanceof BlockItem blockItem) {
             Block block = blockItem.getBlock();
-            if (block instanceof IRotate || block instanceof FurnaceEngineBlock) {
+            if (block instanceof IRotate) {
                 return new KineticStats(block);
             }
         }
@@ -71,24 +58,9 @@ public class DDCreate
 
         REGISTRATE.registerEventListeners(eventBus);
 
-        DDSoundEvents.register(eventBus);
-        DDItemTab.init();
-        DDBlockPartialModel.init();
-        DDBlockEntityTypes.register();
-        DDBlocks.register();
-        DDItems.register();
-        DDFluids.register();
-        DDPaletteBlocks.register();
-
-        DDRecipesTypes.register(eventBus);
-
-        DDTags.init();
-
-        DDConfigs.register(modLoadingContext);
 
         eventBus.addListener(DDCreate::init);
 
-        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> DDCreateClient.onCtorClient(eventBus, forgeEventBus));
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -96,7 +68,6 @@ public class DDCreate
     }
 
     public static void init(final FMLCommonSetupEvent event) {
-        DDFluids.registerFluidInteractions();
     }
 
     public static ResourceLocation asResource(String path) {
