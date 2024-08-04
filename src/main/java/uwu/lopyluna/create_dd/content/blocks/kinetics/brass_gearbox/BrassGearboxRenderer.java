a1863adb945e74000ab3c2d7a7d520d6ff9e805c
Boxxedboi <minefooox@gmail.com>
2024-08-03 18:12:10 -0600

    Fixed Brass Gearbox renderer, brass gearbox instance still needs to be fixed

--- src/main/java/uwu/lopyluna/create_dd/content/blocks/kinetics/brass_gearbox/BrassGearboxRenderer.java
+++ src/main/java/uwu/lopyluna/create_dd/content/blocks/kinetics/brass_gearbox/BrassGearboxRenderer.java
@@ -1,6 +1,7 @@
 package uwu.lopyluna.create_dd.content.blocks.kinetics.brass_gearbox;
 
 import com.jozufozu.flywheel.backend.Backend;
+import com.jozufozu.flywheel.core.PartialModel;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.simibubi.create.AllPartialModels;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
@@ -12,7 +13,10 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import uwu.lopyluna.create_dd.registry.DesiresPartialModels;
+
+import java.util.*;
 
 public class BrassGearboxRenderer extends KineticBlockEntityRenderer<BrassGearboxBlockEntity> {
     public BrassGearboxRenderer(BlockEntityRendererProvider.Context context) {
@@ -37,96 +41,65 @@ public class BrassGearboxRenderer extends KineticBlockEntityRenderer<BrassGearbo
         float angleY = getAngleForTe(be, pos, axisY);
         float angleZ = getAngleForTe(be, pos, axisZ);
 
-        if (!blockState.getValue(BrassGearboxBlock.UP_SHAFT)) {
-            ms.pushPose();
-            CachedBufferer.partial(DesiresPartialModels.TOP_BRASS_PANEL, be.getBlockState())
-                .light(light)
-                .renderInto(ms, buffer.getBuffer(RenderType.solid())
-                );
-            ms.popPose();
-        }
-        if (!blockState.getValue(BrassGearboxBlock.DOWN_SHAFT)) {
-            ms.pushPose();
-            CachedBufferer.partial(DesiresPartialModels.BOTTOM_BRASS_PANEL, be.getBlockState())
-                .light(light)
-                .renderInto(ms, buffer.getBuffer(RenderType.solid())
-                );
-            ms.popPose();
-        }
-        if (!blockState.getValue(BrassGearboxBlock.NORTH_SHAFT)) {
-            ms.pushPose();
-            CachedBufferer.partial(DesiresPartialModels.NORTH_BRASS_PANEL, be.getBlockState())
-                .light(light)
-                .renderInto(ms, buffer.getBuffer(RenderType.solid())
-                );
-            ms.popPose();
-        }
-        if (!blockState.getValue(BrassGearboxBlock.EAST_SHAFT)) {
-            ms.pushPose();
-            CachedBufferer.partial(DesiresPartialModels.EAST_BRASS_PANEL, be.getBlockState())
-                .light(light)
-                .renderInto(ms, buffer.getBuffer(RenderType.solid())
-                );
-            ms.popPose();
-        }
-        if (!blockState.getValue(BrassGearboxBlock.SOUTH_SHAFT)) {
-            ms.pushPose();
-            CachedBufferer.partial(DesiresPartialModels.SOUTH_BRASS_PANEL, be.getBlockState())
-                .light(light)
-                .renderInto(ms, buffer.getBuffer(RenderType.solid())
-                );
-            ms.popPose();
-        }
-        if (!blockState.getValue(BrassGearboxBlock.WEST_SHAFT)) {
-            ms.pushPose();
-            CachedBufferer.partial(DesiresPartialModels.WEST_BRASS_PANEL, be.getBlockState())
-                .light(light)
-                .renderInto(ms, buffer.getBuffer(RenderType.solid())
-                );
-            ms.popPose();
-        }
+        Map<BooleanProperty, PartialModel> brassPanels = new HashMap<>(6);
+        brassPanels.put(BrassGearboxBlock.UP_SHAFT, DesiresPartialModels.TOP_BRASS_PANEL);
+        brassPanels.put(BrassGearboxBlock.DOWN_SHAFT, DesiresPartialModels.BOTTOM_BRASS_PANEL);
+        brassPanels.put(BrassGearboxBlock.NORTH_SHAFT, DesiresPartialModels.NORTH_BRASS_PANEL);
+        brassPanels.put(BrassGearboxBlock.SOUTH_SHAFT, DesiresPartialModels.SOUTH_BRASS_PANEL);
+        brassPanels.put(BrassGearboxBlock.EAST_SHAFT, DesiresPartialModels.EAST_BRASS_PANEL);
+        brassPanels.put(BrassGearboxBlock.WEST_SHAFT, DesiresPartialModels.WEST_BRASS_PANEL);
 
-        if (blockState.getValue(BrassGearboxBlock.UP_SHAFT)) {
-            ms.pushPose();
-            SuperByteBuffer shaft = CachedBufferer.partialFacing(AllPartialModels.SHAFT_HALF, be.getBlockState(), Direction.UP);
-            kineticRotationTransform(shaft, be, axisX, angleY, light);
-            shaft.renderInto(ms, buffer.getBuffer(RenderType.solid()));
-            ms.popPose();
+        List<BooleanProperty> shaftBlockStates = new ArrayList<>(6);
+        shaftBlockStates.add(BrassGearboxBlock.DOWN_SHAFT);
+        shaftBlockStates.add(BrassGearboxBlock.UP_SHAFT);
+        shaftBlockStates.add(BrassGearboxBlock.NORTH_SHAFT);
+        shaftBlockStates.add(BrassGearboxBlock.SOUTH_SHAFT);
+        shaftBlockStates.add(BrassGearboxBlock.WEST_SHAFT);
+        shaftBlockStates.add(BrassGearboxBlock.EAST_SHAFT);
+
+        renderBrassPanels(brassPanels, ms, be, blockState, buffer, light);
+
+        renderShafts(shaftBlockStates, ms, be, blockState, buffer, light);
+
+        shaftBlockStates.clear();
+        brassPanels.clear();
+    }
+
+    private void renderShafts(List<BooleanProperty> shaftBlockStates, PoseStack ms, BrassGearboxBlockEntity be,
+                              BlockState blockState, MultiBufferSource buffer, int light) {
+        BlockPos pos = be.getBlockPos();
+
+        int i = 0;
+        for (Direction direction : Direction.values()) {
+            if (blockState.getValue(shaftBlockStates.get(i))) {
+                Direction.Axis axis = direction.getAxis();
+                float angle = getAngleForTe(be, pos, axis);
+
+                ms.pushPose();
+
+                SuperByteBuffer shaft = CachedBufferer.partialFacing(AllPartialModels.SHAFT_HALF, be.getBlockState(), direction);
+                kineticRotationTransform(shaft, be, axis, angle, light);
+                shaft.renderInto(ms, buffer.getBuffer(RenderType.solid()));
+
+                ms.popPose();
+            }
+            i += 1;
         }
-        if (blockState.getValue(BrassGearboxBlock.DOWN_SHAFT)) {
-            ms.pushPose();
-            SuperByteBuffer shaft = CachedBufferer.partialFacing(AllPartialModels.SHAFT_HALF, be.getBlockState(), Direction.DOWN);
-            kineticRotationTransform(shaft, be, axisX, angleY, light);
-            shaft.renderInto(ms, buffer.getBuffer(RenderType.solid()));
-            ms.popPose();
-        }
-        if (blockState.getValue(BrassGearboxBlock.NORTH_SHAFT)) {
-            ms.pushPose();
-            SuperByteBuffer shaft = CachedBufferer.partialFacing(AllPartialModels.SHAFT_HALF, be.getBlockState(), Direction.NORTH);
-            kineticRotationTransform(shaft, be, axisX, angleZ, light);
-            shaft.renderInto(ms, buffer.getBuffer(RenderType.solid()));
-            ms.popPose();
-        }
-        if (blockState.getValue(BrassGearboxBlock.EAST_SHAFT)) {
-            ms.pushPose();
-            SuperByteBuffer shaft = CachedBufferer.partialFacing(AllPartialModels.SHAFT_HALF, be.getBlockState(), Direction.EAST);
-            kineticRotationTransform(shaft, be, axisX, angleX, light);
-            shaft.renderInto(ms, buffer.getBuffer(RenderType.solid()));
-            ms.popPose();
-        }
-        if (blockState.getValue(BrassGearboxBlock.SOUTH_SHAFT)) {
-            ms.pushPose();
-            SuperByteBuffer shaft = CachedBufferer.partialFacing(AllPartialModels.SHAFT_HALF, be.getBlockState(), Direction.SOUTH);
-            kineticRotationTransform(shaft, be, axisX, angleZ, light);
-            shaft.renderInto(ms, buffer.getBuffer(RenderType.solid()));
-            ms.popPose();
-        }
-        if (blockState.getValue(BrassGearboxBlock.WEST_SHAFT)) {
-            ms.pushPose();
-            SuperByteBuffer shaft = CachedBufferer.partialFacing(AllPartialModels.SHAFT_HALF, be.getBlockState(), Direction.WEST);
-            kineticRotationTransform(shaft, be, axisX, angleX, light);
-            shaft.renderInto(ms, buffer.getBuffer(RenderType.solid()));
-            ms.popPose();
+    }
+
+    private void renderBrassPanels(Map<BooleanProperty, PartialModel> brassPanels, PoseStack ms,
+                                   BrassGearboxBlockEntity be, BlockState blockState, MultiBufferSource buffer, int light) {
+        for (BooleanProperty bool : brassPanels.keySet()) {
+            if (!blockState.getValue(bool)) {
+                ms.pushPose();
+
+                CachedBufferer.partial(brassPanels.get(bool), be.getBlockState())
+                                .light(light)
+                                .renderInto(ms, buffer.getBuffer(RenderType.solid())
+                                );
+
+                ms.popPose();
+            }
         }
     }
 }
